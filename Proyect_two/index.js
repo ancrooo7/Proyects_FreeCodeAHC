e8b8c69273a950c158c8a391de1896bcebe63ffc
Andres <acrespi846@gmail.com>
2020-05-25 11:34:56 -0300

    update Proyects

--- Proyect_two/index.js
+++
@@ -1,8 +0,0 @@
-function addBorders(children) {
-    if (children.length) {
-      for (child of children) {
-        child.setAttribute('style', 'border: 1px solid black;');
-              addBorders(child.children);
-      };
-    }
-  }
